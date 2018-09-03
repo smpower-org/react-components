529b8972b0d79fa4d0ebe5113062cd9d2e09552a
smpower <bzsjxhywrf@outlook.com>
2018-09-03 15:42:20 +0800

    配置路由要显示的组件

--- navigation-router/src/main/views/main.js
+++ navigation-router/src/main/views/main.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import {view as Home} from '../../pages/home/';
+import {view as About} from '../../pages/about/';
+import {view as Topic} from '../../pages/topic/';
 
 import './style.css';
 
 
 const Main = () => (
-	<div className="main">Main Component</div>
+	<div className="main">
+	  <Route exact path="/" component={Home} />
+	  <Route path="/about" component={About} />
+	  <Route path="/topic" component={Topic} />
+	</div>
 );
 
 export default Main;
