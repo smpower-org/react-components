5a21a4b1591a1c6939c5fafe10a902e391128639
smpower <bzsjxhywrf@outlook.com>
2018-09-04 17:43:17 +0800

    初步渲染 markdown 文件内容

--- markdown/src/MarkdownApp.js
+++ markdown/src/MarkdownApp.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import MarkdownIt from 'markdown-it';
+import markdownFile from './markdown.md';
 
-const MarkdownApp = () => (
-  <div>MarkdownApp Component</div>
-);
+const md = new MarkdownIt();
+const result = md.render(markdownFile);
+
+class MarkdownApp extends React.Component {
+	constructor() {
+	  super();
+
+		const md = new MarkdownIt();
+
+		this.state = {
+		  result: md.render(markdownFile)
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="markdown-content">
+					<div dangerouslySetInnerHTML={{__html: this.state.result}}></div>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default MarkdownApp;
